56a15fe5a8b64550b2b85486c302610e24ca59a7
Shailesh Prajapati <shailesh.prajapati@samsara.com>
2025-01-11 23:24:50 +0530

    Update main.go

--- BasicWSClient/main.go
+++ BasicWSClient/main.go
@@ -100,7 +100,7 @@ func main() {
 		}
 	}
 	fmt.Println("connecting...")
-	url := "ws://shailxeta-lor-lb-2093919390.us-west-2.elb.amazonaws.com/ws"
+	url := "ws://<lb_name>.us-west-2.elb.amazonaws.com/ws"
 	if len(os.Args) > 2 {
 		url = os.Args[2]
 	}
